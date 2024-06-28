794f22aa1fdd32ed452c60022cb806ed7c61b9f3
Aniyah Bunn <aniyah@meta.com>
2024-06-28 14:00:21 -0700

    updated user authentication

--- frontend/src/LogInSignUp/LogInSignUp.jsx
+++ frontend/src/LogInSignUp/LogInSignUp.jsx
@@ -35,8 +35,7 @@ const LogInSignUp = () =>{
                     <input type="password" placeholder="Password" />
                 </div>
 
-                {action==="Sign Up"?
-                <div></div>:
+                {action !=="Sign Up" &&
                 <div className="ForgotPassword">Forgot Password <span>Click Here!</span></div>}
                 <div className="submit-container">
                     <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
