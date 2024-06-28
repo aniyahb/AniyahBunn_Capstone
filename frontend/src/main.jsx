d4928a5343987cc6d65fcafaf5bc0987d59f58a7
Aniyah Bunn <aniyah@meta.com>
2024-06-28 13:52:43 -0700

    Work in Progress

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import LogInSignUp from './LogInSignUp/LogInSignUp.jsx'
+import RecipeCard from './RecipeCard/RecipeCard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RecipeCard/>
   </React.StrictMode>,
 )
